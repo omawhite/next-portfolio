a675c86a477e6c247c6390ab5f2b1c1a7200c83a
Omar White <omawhite@indiana.edu>
2021-10-09 17:25:38 -0500

    adds storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    // Paths to the story files
+    "../pages/*.stories.mdx",
+    "../pages/*.stories.js",
+    "../components/*.stories.mdx",
+    "../components/*.stories.js",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
