09c7dd1bd87ca2edbe323ce9563a6ac8922a2c8d
Omar White <omawhite@indiana.edu>
2021-10-09 18:36:54 -0500

    starts layout story

--- components/layout.stories.js
+++ components/layout.stories.js
@@ -3,7 +3,16 @@ import Layout from './layout'
 
 export default {
   component: Layout,
-  title: 'Components/Layout'
+  title: 'Components/Layout',
+  argTypes: {
+    home: {
+      defaultValue: false,
+      control: {type: 'boolean'}
+    }
+  }
 }
 
-export const Default = () => <Layout><p>This is some content</p></Layout>
+const Template = (args) =><Layout {...args}><p>This is some content</p></Layout>
+
+export const Default = Template.bind()
+
