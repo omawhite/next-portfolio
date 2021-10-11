6f3e64ebb12fd91fafebe62ac5f9c0ed94042690
Omar White <omawhite@indiana.edu>
2021-10-11 18:30:39 -0500

    makes sure generateRSSFeed is imported

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
+import { generateRSSFeed } from '../lib/feed'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import Date from '../components/date'
