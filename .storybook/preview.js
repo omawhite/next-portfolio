17697d581fc67eb3beb206b433d8be205186bc99
Omar White <omawhite@indiana.edu>
2021-10-20 19:24:27 -0500

    fixes preview so layout will render, also imports utility styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,16 @@
-// Import the global style enabling tailwind classes
-import '../styles/global.css'
+import '../styles/global.css';
+import '../styles/utils.module.css';
 //overide so that next image component loads properly details https://javascript.plainenglish.io/next-js-storybook-3f8e5a8dab3d
-import * as nextImage from 'next/image'
+import * as NextImage from 'next/image';
+
+// stolen from https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
