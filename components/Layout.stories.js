a9eb8515b0a9924c40482f0483feb15f69e771fe
Omar White <omawhite@indiana.edu>
2021-10-25 19:57:23 -0500

    adds layout back

---
+++ components/Layout.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Layout from './Layout';
+
+export default {
+  component: Layout,
+  title: 'Templates/Layout',
+  argTypes: {
+    home: {
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+  },
+};
+
+const Template = (args) => (
+  <Layout {...args}>
+    <p>This is some content</p>
+  </Layout>
+);
+
+export const Default = Template.bind();
