791300bcf3744c36348184eff321584abd57e832
Omar White <omawhite@indiana.edu>
2021-10-25 20:25:56 -0500

    adds some stuff from the other PR

---
+++ components/BlogPostSnippet.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Date from '../components/Date';
+import utilStyles from '../styles/utils.module.css';
+
+export default function BlogPostSnippet({ id, date, title, content }) {
+  return (
+    <li className={utilStyles.listItem} key={id}>
+      <Link href={`/posts/${id}`}>
+        <a>{title}</a>
+      </Link>
+      <br />
+      <small className={utilStyles.lightText}>
+        <Date dateString={date} />
+      </small>
+    </li>
+  );
+}
