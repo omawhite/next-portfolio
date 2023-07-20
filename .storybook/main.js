9ccf9620c8b79a6a3fe72f2bd3198e74de531770
Omar White <omawhite@indiana.edu>
2023-07-20 12:40:59 -0400

    updates prettier scripts, runs format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,19 @@
 module.exports = {
   stories: [
-  // Paths to the story files
-  '../components/*.stories.mdx', '../components/*.stories.js'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-css-modules-preset'],
+    // Paths to the story files
+    '../components/*.stories.mdx',
+    '../components/*.stories.js',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-css-modules-preset',
+  ],
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
