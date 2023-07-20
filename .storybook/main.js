bf63ca5cd34d743b31dbfb388ea9cfb74c315bae
Omar White <omawhite@indiana.edu>
2023-07-20 12:32:23 -0400

    upgrades storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
   stories: [
-    // Paths to the story files
-    '../components/*.stories.mdx',
-    '../components/*.stories.js',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    'storybook-css-modules-preset',
-  ],
+  // Paths to the story files
+  '../components/*.stories.mdx', '../components/*.stories.js'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-css-modules-preset'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
