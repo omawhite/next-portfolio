e965f195aa546ff248e8ec067b578aa005db8afa
Omar White <omawhite@indiana.edu>
2023-07-20 12:09:23 -0400

    fix casing issues

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
