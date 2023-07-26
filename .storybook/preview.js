fe2a6fff1443031cbfe8b5b15150bbcba08ce8a7
Omar White <omawhite@indiana.edu>
2023-07-26 17:39:21 -0400

    fixes storybook crashes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,5 @@
 import '../styles/global.css';
 import '../styles/utils.module.css';
-//overide so that next image component loads properly details https://javascript.plainenglish.io/next-js-storybook-3f8e5a8dab3d
-import * as NextImage from 'next/image';
-
-// stolen from https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
-const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => <OriginalNextImage {...props} unoptimized />,
-});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
