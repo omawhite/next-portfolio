6f3e879df0dbc498fdc42ff63fa176e68b443082
Omar White <omawhite@indiana.edu>
2023-07-27 20:58:12 -0400

    adds a subject field and some other stuff

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -19,18 +19,45 @@ export default function Contact() {
         >
           <input type="hidden" name="form-name" value="contact" />
           <div>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" required />
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              placeholder="Firstname Lastname"
+            />
           </div>
           <div>
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" required />
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="yourname@yoursite.com"
+            />
           </div>
           <div>
-            <label htmlFor="message">Your message</label>
-            <textarea id="message" name="message" required />
+            <label htmlFor="subject">Subject:</label>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              placeholder="What do you want to talk about?"
+            />
           </div>
-          <button type="submit"> Send </button>
+          <div>
+            <label htmlFor="message">Your message:</label>
+            <textarea
+              id="message"
+              name="message"
+              required
+              placeholder={`What do you want to say?`}
+              spellCheck="true"
+            />
+          </div>
+          <button type="submit">Send</button>
         </form>
       </section>
     </Layout>
