892104fa83d65d3878b809af8381910b18f90163
omawhite <louiswhite3019@gmail.com>
2023-07-29 17:17:37 -0400

    adds some styling to the contact page

--- pages/contact/success.tsx
+++ pages/contact/success.tsx
@@ -1,16 +1,19 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { isContactPageEnabled } from '../../featureFlags';
+import TypographyH1 from '../../components/shadcn/ui/TypographyH1';
+import TypographyP from '../../components/shadcn/ui/TypographyP';
+import TypographyMuted from '../../components/shadcn/ui/TypographyMuted';
 
 const Success: NextPage = () => {
   return (
-    <div>
-      <h1>Form submitted successfully!</h1>
-      <p>{`Thank you for reaching out. I'll get back to you as soon as I can. `}</p>
-      <div>
+    <main className="flex flex-col items-center justify-center p-12">
+      <TypographyH1>Thanks for reaching out.</TypographyH1>
+      <TypographyP>{`I'll get back to you as soon as I can. `}</TypographyP>
+      <TypographyMuted>
         <Link href="/">← Back to home</Link>
-      </div>
-    </div>
+      </TypographyMuted>
+    </main>
   );
 };
 
