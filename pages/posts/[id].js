7d2430018c2269734caa85fafd83dd6364d861e5
omawhite <louiswhite3019@gmail.com>
2023-07-29 22:18:30 -0400

    more wip

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Layout from '../../components/Layout/Layout';
+// import Layout from '../../components/Layout/Layout';
+import Layout from '../../components/Layout/LayoutNew';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 import utilStyles from '../../styles/utils.module.css';
 import Date from '../../components/Date';
+import Link from 'next/link';
 
 export default function Post({ postData }) {
   return (
-    <Layout showBackToHomeLink>
+    <Layout shouldAvatarLinkToHome>
       <Head>
         <title>{postData.title}</title>
         <meta name="og:title" content={postData.title} />
@@ -22,6 +24,9 @@ export default function Post({ postData }) {
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
+      <div className="mt-12">
+        <Link href="/blog"> ← Back to blog</Link>
+      </div>
     </Layout>
   );
 }
