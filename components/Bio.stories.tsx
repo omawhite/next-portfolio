126c01a1b234e6c8efa18dac974f1e82fb3ac9f9
omawhite <louiswhite3019@gmail.com>
2023-07-30 15:30:07 -0400

    refactors bio component, use HomePage content to build homepage

--- components/Bio.stories.tsx
+++ components/Bio.stories.tsx
@@ -30,3 +30,22 @@ export const WithLink: Story = {
     bio: `<p>Hi my name is Omar, I’m a software engineer that specializes in creating great front end experiences, primarily using react. When I’m not engineering I like to game and make music. <a href="https://www.google.com">This is a link to Google</a> </p>`,
   },
 };
+
+export default meta;
+type Story = StoryObj<typeof Bio>;
+
+
+export const Default: Story = {}
+
+export const LongerBio: Story = {
+  args: {
+    bio: `<p>Hi my name is Omar, I’m a software engineer that specializes in creating great front end experiences, primarily using react. When I’m not engineering I like to game and make music.</p>`
+  }
+}
+
+
+export const WithLink: Story = {
+  args: {
+    bio: `<p>Hi my name is Omar, I’m a software engineer that specializes in creating great front end experiences, primarily using react. When I’m not engineering I like to game and make music. <a href="https://www.google.com">This is a link to Google</a> </p>`,
+  },
+  }
