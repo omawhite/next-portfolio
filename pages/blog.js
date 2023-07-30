84fecb0431230bd490084e1270c3f130495eee15
omawhite <louiswhite3019@gmail.com>
2023-07-29 20:31:59 -0400

    swaps blog to new layout

--- pages/blog.js
+++ pages/blog.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
 import { getSortedPostsData } from '../lib/posts';
-import Layout, { siteTitle } from '../components/Layout';
+import Layout from '../components/LayoutNew';
 import BlogPostsList from '../components/BlogPostList';
 import { generateRSSFeed } from '../lib/feed';
 
 export default function Blog({ allPostsData }) {
   return (
-    <Layout showBackToHomeLink>
-      <Head>
-        <title>{siteTitle}</title>
-        <meta name="description" content="A blog by Omar Louis White" />
-      </Head>
+    <Layout shouldAvatarLinkToHome={true}>
       <BlogPostsList postsData={allPostsData} />
     </Layout>
   );
