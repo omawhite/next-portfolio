f57e324cd99bea4fdb87a9b5d16bfb313dcb607e
omawhite <louiswhite3019@gmail.com>
2023-07-30 14:33:48 -0400

    refactors library content, creates new markdown lib file

---
+++ lib/markdown.ts
@@ -0,0 +1,12 @@
+import remark from "remark";
+import html from "remark-html";
+
+/**
+ * Converts markdown content into an html string for rendering
+ * @param {*} markdownContent
+ * @returns
+ */
+export async function markdownContentToHTML(markdownContent) {
+  const processedContent = await remark().use(html).process(markdownContent);
+  return processedContent.toString();
+}
---
+++ lib/pageContent.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { markdownContentToHTML } from './markdown';
+
+const contentDirectory = path.join(process.cwd(), 'content');
+
+export function getHomePageContentData() {
+  const homepageContentFileName = 'home.md';
+  const fullPath = path.join(contentDirectory, homepageContentFileName);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+  const bioContentHtml = markdownContentToHTML(matterResult.data.bioContent || '')
+
+  return {
+    siteTitle: matterResult.data.title,
+    bioContentHtml
+  }
+}
