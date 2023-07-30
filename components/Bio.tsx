b89c68abbf66bc1627300d3299f2528a78fedd60
omawhite <louiswhite3019@gmail.com>
2023-07-30 14:57:36 -0400

    refactors bio component

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -1,8 +1,12 @@
 import TypographyP from '@/components/shadcn/ui/TypographyP';
 
-const bio = `Hi my name is Omar, I’m a software engineer that specializes in creating great front end experiences, primarily using react. When I’m not engineering I like to game and make music.`;
+const defaultBio = `Hi my name is Omar, I’m a software engineer that specializes in creating great front end experiences, primarily using react. When I’m not engineering I like to game and make music.`;
 
-export default function Bio() {
+interface BioProps {
+  bio?: string;
+}
+
+export default function Bio({ bio = defaultBio }: BioProps) {
   return (
     <section className="max-w-xl">
       <TypographyP>{bio}</TypographyP>
