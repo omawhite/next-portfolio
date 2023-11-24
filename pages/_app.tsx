d2e5f31bddae6ff6a9028463f71398538430e693
Omar White <omar.white@cvshealth.com>
2023-11-24 13:37:36 -0500

    fixing a warning from the console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 // docs https://nextjs.org/docs/advanced-features/custom-app
 // next uses the app component to initalize all pages
 
+import Head from 'next/head';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta charSet="UTF-8" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="A site by Omar Louis White" />
