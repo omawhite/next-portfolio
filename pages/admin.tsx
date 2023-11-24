3363e8a9bacdf0f4072f1c2651d6228cd910098e
Omar White <omar.white@cvshealth.com>
2023-11-24 13:20:36 -0500

    things seem to be working at this poitn

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,9 +5,10 @@ const AdminPage = () => {
   return (
     <>
       <Head>
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        <title>Content Manager</title>
       </Head>
       <>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         <Script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js" />
       </>
     </>
