18abed8006ee6621ef424a58cbe04b6e73c290d5
Omar White <omar.white@cvshealth.com>
2023-11-24 14:27:56 -0500

    wip

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,7 +1,12 @@
 import Script from 'next/script';
 import Head from 'next/head';
+import CMS from 'decap-cms-app';
+import { useEffect } from 'react';
 
 const AdminPage = () => {
+  useEffect(() => {
+    CMS.init();
+  }, []);
   return (
     <>
       <Head>
@@ -9,7 +14,7 @@ const AdminPage = () => {
       </Head>
       <>
         <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-        <Script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js" />
+        {/* <Script src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js" /> */}
       </>
     </>
   );
