81c0174050ad546a7f69103079385108a970c4cc
Omar White <omawhite@indiana.edu>
2023-11-25 16:25:06 -0500

    fix(cms): swaps from content back to body in cms config

--- src/CMSConfig.ts
+++ src/CMSConfig.ts
@@ -42,7 +42,7 @@ const CMSConfig: Config = {
         },
         {
           label: 'Content',
-          name: 'content',
+          name: 'body',
           widget: 'markdown',
         },
       ],
