28600bc922edb02c6f8246b12b282970f2f289a7
Omar White <omawhite@indiana.edu>
2023-11-26 17:28:28 -0500

    fix: moves viewport meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,15 @@
 // next uses the app component to initalize all pages
 
 import '@/styles/global.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
