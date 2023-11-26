4444d35dbe934bfcf9313d79628350211a61a98f
Omar White <omawhite@indiana.edu>
2023-11-25 22:23:13 -0500

    adds an additional post with different format

--- src/components/__tests__/postsData.fixture.ts
+++ src/components/__tests__/postsData.fixture.ts
@@ -66,4 +66,15 @@ export const postDataList = [
     tags: ['My edits'],
     title: 'How I Ended Up A Software Engineer',
   },
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '"2023-11-20"',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer5',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
 ];
