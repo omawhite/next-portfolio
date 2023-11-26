7422f69d2e56e115d2cd0c28d3cae3d25418d2b9
Omar White <omawhite@indiana.edu>
2023-11-25 21:52:58 -0500

    hopefully this fixes chromatic

---
+++ src/components/__tests__/postsData.fixture.ts
@@ -0,0 +1,69 @@
+export const postData = {
+  content:
+    'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete...',
+
+  date: '"2023-11-20T00:00:00.000Z"',
+  description: 'A story about my journey to becoming a software engineer.',
+  featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+  id: 'how-i-ended-up-a-software-engineer',
+  tags: ['My edits'],
+  title: 'How I Ended Up A Software Engineer',
+};
+
+export const postDataList = [
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '2018-02-11',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '2018-02-11',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer2',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '2018-02-11',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer3',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '2018-02-11',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer4',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
+  {
+    content:
+      'The other day I was cleaning out my inbox when I came across a message from [Code Academy](https://www.codecademy.com/). I quickly delete…',
+
+    date: '2018-02-11',
+    description: 'A story about my journey to becoming a software engineer.',
+    featuredimage: '/img/5ff6e03f-0ffe-4606-855e-5dfee4556f44.jpeg',
+    id: 'how-i-ended-up-a-software-engineer5',
+    tags: ['My edits'],
+    title: 'How I Ended Up A Software Engineer',
+  },
+];
