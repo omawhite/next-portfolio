fe6d6a8d8c4fc3c19d4bdbaa9c447c1c2a465301
Omar Louis White <louiswhite3019@gmail.com>
2023-12-12 22:24:37 -0500

    feat: adds a feature flag gatting the contact page when deployed to vercel (#60)

--- src/featureFlags.ts
+++ src/featureFlags.ts
@@ -1,1 +1,4 @@
 export const isProduction = process.env.NODE_ENV === 'production';
+export const isVercel = ['production', 'preview', 'development'].includes(
+  process.env.NEXT_PUBLIC_VERCEL_ENV ?? ''
+);
