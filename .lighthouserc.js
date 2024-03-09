efc7af3e56e41500ae3a996a107434e4fd10277c
Omar White <omawhite@indiana.edu>
2024-03-09 17:31:55 -0500

    fix: adds urls to run lighthouse against

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,6 +7,11 @@ module.exports = {
       preset: 'lighthouse:no-pwa',
     },
     collect: {
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/contact',
+        'http://localhost:3000/blog',
+      ],
       startServerCommand: 'npm run start',
     },
   },
