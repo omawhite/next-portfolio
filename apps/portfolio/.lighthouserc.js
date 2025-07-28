94d070b7ba31b1c3478f6e9e08c5468826fef63c
Omar White <omar.white@cvshealth.com>
2025-07-28 17:08:04 -0400

    fix: filter lighthouse start script by portfolio app

--- apps/portfolio/.lighthouserc.js
+++ apps/portfolio/.lighthouserc.js
@@ -10,7 +10,8 @@ module.exports = {
         'http://localhost:3000/contact',
         'http://localhost:3000/blog',
       ],
-      startServerCommand: 'HOSTNAME=localhost pnpm turbo start',
+      startServerCommand:
+        'HOSTNAME=localhost pnpm turbo start --filter=next_porfolio',
     },
   },
 };
