d06db2ae091d0e769fefd329e9f37c3af6cd4f6e
Omar Louis White <louiswhite3019@icloud.com>
2025-08-07 20:32:43 -0400

    fix: use proper path for preview

--- apps/portfolio/tina/config.ts
+++ apps/portfolio/tina/config.ts
@@ -70,8 +70,8 @@ export default defineConfig({
           },
         ],
         ui: {
-          // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+          //makes it so that the preview shows my post in context of the site
+          router: ({ document }) => `/posts/${document._sys.filename}`,
         },
       },
       {
